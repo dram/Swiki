ac0b50579bae2dd708c7d7d84ee6c3bb29cf046b
Xin Wang <dram.wang@gmail.com>
2022-05-15 11:51:10 +0800

    Remove patch and extension code of NetNameResolver

--- Swiki.cs
+++ Swiki.cs
@@ -1,4 +1,4 @@
-﻿'From Squeak6.0alpha of 6 May 2022 [latest update: #21736] on 15 May 2022 at 11:12:33 am'!
+﻿'From Squeak6.0alpha of 6 May 2022 [latest update: #21736] on 15 May 2022 at 11:40 am'!
 Object subclass: #AniAccess
 	instanceVariableNames: 'allLevel usersLevel groupToLevel'
 	classVariableNames: ''
@@ -2193,39 +2193,6 @@ pluginText: text request: request response: response shelf: shelf book: book pag
 initialize
 	CR _ Character cr.
 	LF _ Character lf! !
-
-
-!NetNameResolver class methodsFor: 'lookups'!
-addressForName: hostName timeout: secs
-	"Look up the given host name and return its address. Return nil if the address is not found in the given number of seconds."
-	"NetNameResolver addressForName: 'create.ucsb.edu' timeout: 30"
-	"NetNameResolver addressForName: '100000jobs.de' timeout: 30"
-	"NetNameResolver addressForName: '1.7.6.4' timeout: 30"
-	"NetNameResolver addressForName: '' timeout: 30 (This seems to return nil?)"
-
-	^self addressForName: hostName timeout: secs ifTimeout: [(NameLookupFailure hostName: hostName) signal: 'Could not resolve the server named: ', hostName]! !
-
-!NetNameResolver class methodsFor: 'lookups'!
-addressForName: hostName timeout: secs ifTimeout: block
-	| deadline result |
-	self initializeNetwork.
-	"check if this is a valid numeric host address (e.g. 1.2.3.4)"
-	result _ self addressFromString: hostName.
-	result isNil ifFalse: [^result].
-
-	"Look up a host name, including ones that start with a digit (e.g. 100000jobs.de or squeak.org)"
-	deadline _ Time millisecondClockValue + (secs * 1000).
-	"Protect the execution of this block, as the ResolverSemaphore is used for both parts of the transaction."
-	self resolverMutex
-		critical: [
-			(self waitForResolverReadyUntil: deadline)
-				ifTrue: [
-					self primStartLookupOfName: hostName.
-					(self waitForCompletionUntil: deadline)
-						ifTrue: [result _ self primNameLookupResult]
-						ifFalse: [block value]]
-				ifFalse: [block value]].
-	^result! !
 
 
 !PositionableStream methodsFor: 'string functions'!
